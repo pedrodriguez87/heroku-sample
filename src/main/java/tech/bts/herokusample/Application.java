2af08f457798d647181c453a50bd76efb09ebc32
Absolutt87 <pedrodriguez87@hotmail.com>
2019-05-04 18:43:20 +0200

    creating enviromental variables local and in heroku

--- src/main/java/tech/bts/herokusample/Application.java
+++ src/main/java/tech/bts/herokusample/Application.java
@@ -5,13 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) { SpringApplication.run(Application.class, args); }
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
-
+    /** This will be executed when we start the application. */
     @Bean
     public CommandLineRunner test() {
 
